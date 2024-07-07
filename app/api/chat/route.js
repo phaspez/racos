1749c8c7782a802fc09ce03dbd1de44b7f2cc4ee
Phaspez <82428635+phaspez@users.noreply.github.com>
2024-07-07 17:26:45 +0700

    fix env

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,11 +1,18 @@
-import { google } from "@ai-sdk/google";
+//import { google } from "@ai-sdk/google";
 import { streamText } from "ai";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
 
 export async function POST(req) {
+	"use server";
+
+	const google = createGoogleGenerativeAI({
+		apiKey: process.env.API_KEY,
+	});
+
 	const { messages } = await req.json();
 
 	const result = await streamText({
-		model: google("models/gemini-1.5-pro-latest"),
+		model: google("models/gemini-1.5-flash-latest"),
 		messages,
 	});
 
