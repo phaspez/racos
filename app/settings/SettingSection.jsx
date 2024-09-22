e34157334276a9b3666283867b9aa060a99bcbb9
Phaspez <82428635+phaspez@users.noreply.github.com>
2024-09-22 17:04:35 +0700

    quick fix h4 tags appear black on dark mode

--- app/settings/SettingSection.jsx
+++ app/settings/SettingSection.jsx
@@ -2,7 +2,7 @@ export default function SettingSection({ title, children }) {
 	return (
 		<div className="flex p-2 max-w-4xl bg-white dark:bg-gray-600 shadow-lg rounded-lg">
 			<div className="grow w-full">
-				<h4 className="grow">{title}</h4>
+				<h4 className="grow dark:text-white">{title}</h4>
 				{children}
 			</div>
 		</div>
