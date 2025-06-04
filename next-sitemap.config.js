cc2743c1df688e89e269534a1e016d3525d41333
mintram <tramtrimin@gmail.com>
2025-06-04 18:36:46 +0700

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-	siteUrl: process.env.SITE_URL || "https://your-domain.com",
+	siteUrl: process.env.SITE_URL || "https://caas.mintram.id.vn",
 	generateRobotsTxt: true, // generates robots.txt
 	robotsTxtOptions: {
 		policies: [
